b2aefe437a0d13cebcdf5cbb3a06a7644724fec0
ibtsamali1 <ibtsam0408@gmail.com>
2026-05-01 05:42:00 +0500

    neondb added

--- src/chat_rag/settings.py
+++ src/chat_rag/settings.py
@@ -134,12 +134,23 @@ WSGI_APPLICATION = 'chat_rag.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+CONN_MAX_AGE=config("CONN_MAX_AGE",cast=int,default=30)
+DATABASE_URL=config("DATABASE_URL", default=None)
 
+if DATABASE_URL is not None:
+    import dj_database_url
+    DATABASES = {
+        "default": dj_database_url.config(default=DATABASE_URL,
+        conn_health_checks=True,
+        conn_max_age=CONN_MAX_AGE,
+        )
+
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
