52425ccfa196059e9cdb979b670f7ad35c3c6a67
ibtsamali1 <ibtsam0408@gmail.com>
2026-05-03 02:14:24 +0500

    req added

--- src/chat_rag/settings.py
+++ src/chat_rag/settings.py
@@ -20,13 +20,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^m2hrjs-kyqx)sh6im2=k&_s-nqn&s9ihl$k-b9t3csyg4#=wi'
+
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    ".railway.app" # https://saas.prod.railway.app
+]
+if DEBUG:
+    ALLOWED_HOSTS += [
+        "127.0.0.1",
+        "localhost"
+    ]
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
